172acc41449ea3420aeaf5ad201f568d51f54f69
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2021-12-06 11:46:24 +0100

    Refactor in c# style

--- 1-Properties/Card.cs
+++ 1-Properties/Card.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Properties
 {
     using System;
@@ -7,9 +9,9 @@ namespace Properties
     /// </summary>
     public class Card
     {
-        private readonly string seed;
-        private readonly string name;
-        private readonly int ordinal;
+        public string Seed { get; }
+        public string Name { get; }
+        public int Ordinal { get; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Card"/> class.
@@ -19,9 +21,9 @@ namespace Properties
         /// <param name="ordinal">the ordinal number of the card.</param>
         public Card(string name, string seed, int ordinal)
         {
-            this.name = name;
-            this.ordinal = ordinal;
-            this.seed = seed;
+            Name = name;
+            Ordinal = ordinal;
+            Seed = seed;
         }
 
         /// <summary>
@@ -30,35 +32,38 @@ namespace Properties
         /// <param name="tuple">the informations about the card as a tuple.</param>
         internal Card(Tuple<string, string, int> tuple) : this(tuple.Item1, tuple.Item2, tuple.Item3)
         {
-        }
-
-        // TODO improve
-        public string GetSeed()
-        {
-            return this.seed;
-        }
-
-        // TODO improve
-        public string GetName()
-        {
-            return this.name;
-        }
-
-        // TODO improve
-        public int GetOrdinal()
-        {
-            return this.ordinal;
+            Name = tuple.Item1;
+            Seed = tuple.Item2;
+            Ordinal = tuple.Item3;
         }
 
         /// <inheritdoc cref="object.ToString"/>
         public override string ToString()
         {
             // TODO understand string interpolation
-            return $"{this.GetType().Name}(Name={this.GetName()}, Seed={this.GetSeed()}, Ordinal={this.GetOrdinal()})";
+            return $"{this.GetType().Name}(Name={Name}, Seed={Seed}, Ordinal={Ordinal})";
         }
 
         // TODO generate Equals(object obj)
 
         // TODO generate GetHashCode()
+        private sealed class SeedNameEqualityComparer : IEqualityComparer<Card>
+        {
+            public bool Equals(Card x, Card y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return x.Seed == y.Seed && x.Name == y.Name;
+            }
+
+            public int GetHashCode(Card obj)
+            {
+                return HashCode.Combine(obj.Seed, obj.Name);
+            }
+        }
+
+        public static IEqualityComparer<Card> SeedNameComparer { get; } = new SeedNameEqualityComparer();
     }
 }
